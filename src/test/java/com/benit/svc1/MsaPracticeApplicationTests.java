048f4f8bb149ee6a663154c8a342e42535cca06c
soby <vthquddbsv@gmail.com>
2025-03-10 23:11:30 +0900

    Initial commit

---
+++ src/test/java/com/benit/svc1/MsaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benit.svc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
