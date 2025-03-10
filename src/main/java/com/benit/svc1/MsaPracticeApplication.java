8699b9fac056c390983b04d55b95960088260583
soby <vthquddbsv@gmail.com>
2025-03-10 23:15:52 +0900

    fix

--- src/main/java/com/benit/svc1/MsaPracticeApplication.java
+++ src/main/java/com/benit/svc1/MsaPracticeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsaPracticeApplication {
-
+//	first service 실행
 	public static void main(String[] args) {
 		SpringApplication.run(MsaPracticeApplication.class, args);
 	}
